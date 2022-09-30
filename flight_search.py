bed29de88c44e3185400876db18d89d4ece38463
Umole Andrew <39883080+umole@users.noreply.github.com>
2022-09-30 23:58:12 +0100

    Add files via upload

--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,5 @@
+import requests
+
 from data_manager import DataManager
 
 TEQUILA_ENDPOINT = "https://api.tequila.kiwi.com"
@@ -6,5 +8,16 @@ TEQUILA_API_KEY = "07Xwr8MArsKJh7F3S-gBYGfWhfmhymTV"
 
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-
+    def get_destination_code(self, city_name):
+        location_url = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {
+            "apikey": TEQUILA_ENDPOINT
+        }
+        query = {
+            "term": city_name,
+            "location_types": "airport",
+        }
+        response = requests.get(url=location_url, headers=headers, params=query)
+        flight_search_result = response.json()
+        return flight_search_result
 
